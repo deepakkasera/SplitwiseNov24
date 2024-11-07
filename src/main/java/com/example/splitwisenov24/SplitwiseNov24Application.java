51e9edf87c35d66b1386e8083d47742ed270e423
deepakkasera <deepak.kasera143.dk@gmail.com>
2024-11-07 22:06:30 +0530

    Splitwise

---
+++ src/main/java/com/example/splitwisenov24/SplitwiseNov24Application.java
@@ -0,0 +1,13 @@
+package com.example.splitwisenov24;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitwiseNov24Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitwiseNov24Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/splitwisenov24/strategy/HeapSettleUpStrategy.java
@@ -0,0 +1,14 @@
+package com.example.splitwisenov24.strategy;
+
+import com.example.splitwisenov24.models.Expense;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HeapSettleUpStrategy implements SettleUpStrategy {
+    @Override
+    public List<Expense> settleUp(List<Expense> expenses) {
+        //Implement this algorithm using Min and Max Heap.
+        return new ArrayList<>();
+    }
+}
